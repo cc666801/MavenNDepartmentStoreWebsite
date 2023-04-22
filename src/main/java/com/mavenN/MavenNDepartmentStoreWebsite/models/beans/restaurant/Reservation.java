23dd975e72ce95c951dcb90880097003977adf7e
syun lin <leo4042802@gmail.com>
2023-04-22 21:19:30 +0800

    訂位表欄位完成

--- src/main/java/com/mavenN/MavenNDepartmentStoreWebsite/models/beans/restaurant/Reservation.java
+++ src/main/java/com/mavenN/MavenNDepartmentStoreWebsite/models/beans/restaurant/Reservation.java
@@ -2,10 +2,13 @@ package com.mavenN.MavenNDepartmentStoreWebsite.models.beans.restaurant;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
@@ -13,34 +16,145 @@ import javax.persistence.TemporalType;
 import org.springframework.data.annotation.Id;
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.mavenN.MavenNDepartmentStoreWebsite.models.beans.companySystem.Company;
+
 @Entity
 @Table(name="restaurant_Reservation")
 public class Reservation {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer r_id;
+	private Integer r_id;	
 	
 	@Column(columnDefinition = "nvarchar(50) NOT NULL")
 	private String name; 
 	
+	@Column(columnDefinition = "nvarchar(50)")
 	private String telephone;
 	
+	@Column(columnDefinition = "nvarchar(50)")
 	private String email;
 	
+	@Column(columnDefinition = "nvarchar(50)")
 	private String remark;
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name="fk_company_id")
+	private Company company;
 
+	@Column(nullable = false)
 	@DateTimeFormat(pattern = "yyyy/MM/dd")
 	@Temporal(TemporalType.DATE)
 	private Date date;
 	
+	@Column(columnDefinition = "nvarchar(50) NOT NULL")
 	private String time_interval;
 	
+	@Column(columnDefinition = "nvarchar(50) NOT NULL")
 	private String time;
 	
+	@Column(columnDefinition = "int NOT NULL")
 	private Integer adult;
 	
+	@Column(columnDefinition = "int NOT NULL")
 	private Integer children;
+
+	// Constructor
+	public Reservation() {
+		super();
+	}
+	
+	// getter & setter
+	public Integer getR_id() {
+		return r_id;
+	}
+
+	public void setR_id(Integer r_id) {
+		this.r_id = r_id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public Company getCompany() {
+		return company;
+	}
+
+	public void setCompany(Company company) {
+		this.company = company;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getTime_interval() {
+		return time_interval;
+	}
+
+	public void setTime_interval(String time_interval) {
+		this.time_interval = time_interval;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public Integer getAdult() {
+		return adult;
+	}
+
+	public void setAdult(Integer adult) {
+		this.adult = adult;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+	
+	
+	
 	
 	
 }
