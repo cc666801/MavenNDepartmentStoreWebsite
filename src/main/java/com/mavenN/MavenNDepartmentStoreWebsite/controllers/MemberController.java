784f548deb8e16885eb2645f982c48ee3db20671
DESKTOP-HTSRBH9\zack1 <zack123129@gmail.com>
2023-04-25 00:00:06 +0800

    小修改(資料進入變null)

--- src/main/java/com/mavenN/MavenNDepartmentStoreWebsite/controllers/MemberController.java
+++ src/main/java/com/mavenN/MavenNDepartmentStoreWebsite/controllers/MemberController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mavenN.MavenNDepartmentStoreWebsite.models.beans.memberSystem.MemberFavorite;
 import com.mavenN.MavenNDepartmentStoreWebsite.models.beans.memberSystem.MemberLevel;
@@ -18,12 +20,14 @@ import com.mavenN.MavenNDepartmentStoreWebsite.models.beans.memberSystem.MemberS
 import com.mavenN.MavenNDepartmentStoreWebsite.models.beans.memberSystem.MemberSystem;
 import com.mavenN.MavenNDepartmentStoreWebsite.models.services.MemberService;
 
+@Controller
 public class MemberController {
 
 	@Autowired
 	private MemberService memberService;
 
 	// 新增會員
+	@ResponseBody
 	@PostMapping("/newmember")
 	public ResponseEntity<Void> createMemberSystem(@RequestBody MemberSystem memberSystem) {
 		memberService.createMemberSystem(memberSystem);
@@ -31,6 +35,7 @@ public class MemberController {
 	}
 
 	// 搜索單筆會員
+	@ResponseBody
 	@GetMapping("/member/{id}")
 	public ResponseEntity<MemberSystem> getMemberSystemById(@PathVariable Integer id) {
 		MemberSystem memberSystem = memberService.getMemberSystemById(id);
@@ -38,6 +43,7 @@ public class MemberController {
 	}
 
 	// 搜索所有會員
+	@ResponseBody
 	@GetMapping("/allmember")
 	public ResponseEntity<List<MemberSystem>> getAllMemberSystems() {
 		List<MemberSystem> memberSystems = memberService.getAllMemberSystems();
@@ -45,6 +51,7 @@ public class MemberController {
 	}
 
 	// 更新會員資料
+	@ResponseBody
 	@PutMapping("/updatemember")
 	public ResponseEntity<Void> updateMemberSystem(@RequestBody MemberSystem memberSystem) {
 		memberService.updateMemberSystem(memberSystem);
@@ -52,6 +59,7 @@ public class MemberController {
 	}
 
 	// 刪除會員資料
+	@ResponseBody
 	@DeleteMapping("/deletemember/{id}")
 	public ResponseEntity<Void> deleteMemberSystem(@PathVariable Integer id) {
 		memberService.deleteMemberSystem(id);
@@ -61,6 +69,7 @@ public class MemberController {
 //-------------------------------------------------------------------------
 
 	// 新增會員最愛資料
+	@ResponseBody
 	@PostMapping("/newmemberfavorite")
 	public ResponseEntity<Void> createMemberFavorite(@RequestBody MemberFavorite memberFavorite) {
 	    memberService.createMemberFavorite(memberFavorite);
@@ -68,6 +77,7 @@ public class MemberController {
 	}
 	
 	// 刪除會員最愛資料
+	@ResponseBody
 	@DeleteMapping("/deletememberfavorite/{id}")
 	public ResponseEntity<Void> deleteMemberFavorite(@PathVariable Integer id) {
 	    memberService.deleteMemberFavorite(id);
@@ -75,6 +85,7 @@ public class MemberController {
 	}
 	
 	// 搜尋會員所有最愛資料
+	@ResponseBody
 	@GetMapping("/allmemberfavorite/{memberAccount}")
 	public ResponseEntity<List<MemberFavorite>> getMemberFavoritesByAccount(@PathVariable String memberAccount) {
 		List<MemberFavorite> memberFavorites = memberService.getMemberFavoritesByAccount(memberAccount);
@@ -84,6 +95,7 @@ public class MemberController {
 //-------------------------------------------------------------------------
 	
 	// 新增會員等級
+	@ResponseBody
 	@PostMapping("/newmemberlevel")
 	public ResponseEntity<Void> createMemberLevel(@RequestBody MemberLevel memberLevel) {
 		memberService.createMemberLevel(memberLevel);
@@ -91,6 +103,7 @@ public class MemberController {
 	}
 
 	// 搜尋會員等級資料
+	@ResponseBody
 	@GetMapping("/memberlevel/{memberAccount}")
 	public ResponseEntity<List<MemberLevel>> findMemberLevelByMemberAccount(@PathVariable String memberAccount) {
 		List<MemberLevel> memberLevels = memberService.findMemberLevelByMemberAccount(memberAccount);
@@ -98,6 +111,7 @@ public class MemberController {
 	}
 
 	// 更新會員等級
+	@ResponseBody
 	@PutMapping("/updatememberlevel")
 	public ResponseEntity<Void> updateMemberLevel(@RequestBody MemberLevel memberLevel) {
 		memberService.updateMemberLevel(memberLevel);
@@ -107,6 +121,7 @@ public class MemberController {
 //-------------------------------------------------------------------------
 
 	// 新增會員違規紀錄
+	@ResponseBody
 	@PostMapping("/newmembershopviolation")
 	public ResponseEntity<Void> createMemberShopViolation(@RequestBody MemberShopViolation memberShopViolation) {
 		memberService.createMemberShopViolation(memberShopViolation);
@@ -114,6 +129,7 @@ public class MemberController {
 	}
 
 	// 查詢會員違規紀錄
+	@ResponseBody
 	@GetMapping("/membershopviolation/{memberAccount}")
 	public ResponseEntity<List<MemberShopViolation>> getMemberShopViolationsByAccount(
 			@PathVariable String memberAccount) {
@@ -123,13 +139,15 @@ public class MemberController {
 	}
 
 	// 更新違規紀錄
+	@ResponseBody
 	@PutMapping("/updatemembershopviolation")
 	public ResponseEntity<Void> updateMemberShopViolation(@RequestBody MemberShopViolation memberShopViolation) {
 		memberService.updateMemberShopViolation(memberShopViolation);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
 
-	// 刪除違規紀錄:
+	// 刪除違規紀錄
+	@ResponseBody
 	@DeleteMapping("/deletemembershopviolation/{id}")
 	public ResponseEntity<Void> deleteMemberShopViolation(@PathVariable Integer id) {
 		memberService.deleteMemberShopViolation(id);
