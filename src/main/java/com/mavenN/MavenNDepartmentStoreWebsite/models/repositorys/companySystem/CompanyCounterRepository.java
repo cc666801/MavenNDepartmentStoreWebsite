87039f81337edf235a94cd05cafab76936f34c44
cc666801 <cc666801@gmail.com>
2023-04-28 00:53:04 +0800

    Purpose: Update Query in Repository and add pictures, prepare to update counter frontend on 2023/4/28

--- src/main/java/com/mavenN/MavenNDepartmentStoreWebsite/models/repositorys/companySystem/CompanyCounterRepository.java
+++ src/main/java/com/mavenN/MavenNDepartmentStoreWebsite/models/repositorys/companySystem/CompanyCounterRepository.java
@@ -1,10 +1,18 @@
 package com.mavenN.MavenNDepartmentStoreWebsite.models.repositorys.companySystem;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.mavenN.MavenNDepartmentStoreWebsite.models.beans.companySystem.CompanyCounter;
 import com.mavenN.MavenNDepartmentStoreWebsite.models.beans.companySystem.CompanyCounterId;
 
 public interface CompanyCounterRepository extends JpaRepository<CompanyCounter, CompanyCounterId>{
-
+	@Query("SELECT cc FROM CompanyCounter cc WHERE cc.counter.property = :propertyValue")
+	List<CompanyCounter> findByCounterProperty(@Param("propertyValue") String counterFloor);
+	
+	List<CompanyCounter> findByOnCounterTimeGreaterThanEqualAndOffCounterTimeLessThanEqual(Date startTime, Date endTime);
 }
--- src/main/java/com/mavenN/MavenNDepartmentStoreWebsite/models/repositorys/companySystem/CompanyRepository.java
+++ src/main/java/com/mavenN/MavenNDepartmentStoreWebsite/models/repositorys/companySystem/CompanyRepository.java
@@ -2,7 +2,6 @@ package com.mavenN.MavenNDepartmentStoreWebsite.models.repositorys.companySystem
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.mavenN.MavenNDepartmentStoreWebsite.models.beans.Message;
 import com.mavenN.MavenNDepartmentStoreWebsite.models.beans.companySystem.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Integer>{
